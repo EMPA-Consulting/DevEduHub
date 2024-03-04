2e562235f7b45d877f5110621a7c3aaafb947857
mariagimenezbustos <134734638+mariagimenezbustos@users.noreply.github.com>
2024-03-04 11:43:01 +0100

    testing if .env is ignored

--- master-nextjs/mariagimenezbustos/myspace/app/blog/[slug]/page.tsx
+++ master-nextjs/mariagimenezbustos/myspace/app/blog/[slug]/page.tsx
@@ -1,8 +1,19 @@
+export const revalidate = 1200;
 
 interface Post {
     title: string;
     content: string;
     slug: string;
+}
+
+export async function generateStaticParams() {
+    const posts: Post[] = await fetch('http://localhost:3000/api/content').then(
+        (res) => res.json()
+    );
+    
+    return posts.map((post) => ({
+        slug: post.slug,
+    }));
 }
 
 interface Props {
