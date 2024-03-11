3dcd6a7ce1db9ea00bd3a7b5e369a0bdd5c5d0c3
Maria Gimenez Bustos <134734638+mariagimenezbustos@users.noreply.github.com>
2024-03-11 12:55:42 +0100

    displaying posts and styling

--- t3-stack/myt3app/.eslintrc.cjs
+++ t3-stack/myt3app/.eslintrc.cjs
@@ -1,21 +1,61 @@
+// /** @type {import("eslint").Linter.Config} */
+// const config = {
+//   parser: "@typescript-eslint/parser",
+//   parserOptions: {
+//     project: true,
+//   },
+//   plugins: ["@typescript-eslint"],
+//   extends: [
+//     "next/core-web-vitals",
+//     "plugin:@typescript-eslint/recommended-type-checked",
+//     "plugin:@typescript-eslint/stylistic-type-checked",
+//   ],
+//   rules: {
+//     // These opinionated rules are enabled in stylistic-type-checked above.
+//     // Feel free to reconfigure them to your own preference.
+//     "@typescript-eslint/array-type": "off",
+//     "@typescript-eslint/consistent-type-definitions": "off",
+
+//     "@typescript-eslint/consistent-type-imports": [
+//       "warn",
+//       {
+//         prefer: "type-imports",
+//         fixStyle: "inline-type-imports",
+//       },
+//     ],
+//     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+//     "@typescript-eslint/require-await": "off",
+//     "@typescript-eslint/no-misused-promises": [
+//       "error",
+//       {
+//         checksVoidReturn: { attributes: false },
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = config;
+
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: "tsconfig.json",
+      },
+    },
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
+    project: "./tsconfig.json",
   },
   plugins: ["@typescript-eslint"],
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-  ],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
@@ -24,13 +64,6 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        checksVoidReturn: { attributes: false },
-      },
-    ],
   },
 };
 
