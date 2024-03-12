934122eef24db339949c5d7d7564deadb2180ff7
Maria Gimenez Bustos <134734638+mariagimenezbustos@users.noreply.github.com>
2024-03-12 12:24:47 +0100

    static props and server side helpers

--- t3-stack/myt3app/src/pages/_app.tsx
+++ t3-stack/myt3app/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -15,6 +16,11 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <main className={`font-sans ${inter.variable}`}>
       <ClerkProvider {...pageProps}>
+        <Head>
+          <title>EMPA-X</title>
+          <meta name="description" content="💭" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         < Toaster position="bottom-center" />
         <Component {...pageProps} />
       </ClerkProvider>
