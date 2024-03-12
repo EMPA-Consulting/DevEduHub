163f2cdb343d57a2dbf91bbb06e2f4b14f5d55a2
Maria Gimenez Bustos <134734638+mariagimenezbustos@users.noreply.github.com>
2024-03-12 10:17:04 +0100

    emoji posting is working

--- t3-stack/myt3app/src/server/api/routers/posts.ts
+++ t3-stack/myt3app/src/server/api/routers/posts.ts
@@ -3,9 +3,9 @@
 import { clerkClient } from "@clerk/nextjs/server";
 import type { User } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
-// import { z } from "zod";
+import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -19,6 +19,9 @@ export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.db.post.findMany({
       take: 100,
+      orderBy: [
+        { createdAt: "desc" }
+      ],
     });
 
     const users = (
@@ -43,5 +46,22 @@ export const postsRouter = createTRPCRouter({
         },
       }
     })
+  }),
+
+  create: privateProcedure.input(
+    z.object({
+      content: z.string().emoji().min(1).max(280),
+    })
+  ).mutation(async ({ ctx, input }) => {
+    const authorId = ctx.userId;
+
+    const post = await ctx.db.post.create({
+      data: {
+        authorId,
+        content: input.content,
+      },
+    });
+
+    return post;
   }),
 });
