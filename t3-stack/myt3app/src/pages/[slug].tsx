bad42045cb4be4aa4cb75967800d4d5cd69088e7
Maria Gimenez Bustos <134734638+mariagimenezbustos@users.noreply.github.com>
2024-03-13 09:37:11 +0100

    profile page style up

--- t3-stack/myt3app/src/pages/[slug].tsx
+++ t3-stack/myt3app/src/pages/[slug].tsx
@@ -16,11 +16,25 @@ const ProfilePage: NextPage<{username: string}> = ({username}) => {
         <title>{data.username}</title>
       </Head>
 
-      <main className="flex justify-center h-screen">
-        <div>
-          {data.username}
+      <PageLayout>
+        <div className="relative h-36 bg-slate-600">
+          <Image 
+            src={data.profilePicture} 
+            alt={`@${data.username}'s profile picture`} 
+            width={128} 
+            height={128} 
+            className="-mb-[64px] absolute bottom-0 left-0 ml-4 rounded-full border-4 border-black bg-black"
+          />
         </div>
-      </main>
+
+        <div className="h-[64px]"></div>
+
+        <div className="p-4 text-2xl font-bold">
+          {`@${data.username}`}
+        </div>
+
+        <div className="w-full border-b border-slate-400" />
+      </PageLayout>
     </>
   );
 };
@@ -29,6 +43,8 @@ import { createServerSideHelpers } from '@trpc/react-query/server';
 import { appRouter } from "~/server/api/root";
 import { db } from "~/server/db";
 import superjson from "superjson";
+import { PageLayout } from "~/components/layout";
+import Image from "next/image";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createServerSideHelpers({
